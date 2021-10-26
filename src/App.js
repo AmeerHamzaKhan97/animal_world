4d2eb4902ef2b86c7f9739e97d7cf76a58370f29
AmeerHamzaKhan97 <hamzakn97@gmail.com>
2021-10-26 22:13:49 +0530

    change

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import firstImg from "./images/firstImg.webp";
 import animalWorld1 from './images/animal world-1.webp'
 import secondImg from './images/secondImg.webp'
+import thegame from './images/THE GAME (1).webp'
 
 function App() {
   return (
@@ -17,15 +18,37 @@ function App() {
           <div class="text-on-image">
             {/* <h3> Welcome to my Blog </h3>
             <p> FREEEEDOM </p> */}
-            <img src={animalWorld1 } alt="Freedom Blog" />
+            <img src={animalWorld1} alt="Freedom Blog" />
           </div>
         </div>
       </header>
       <div className="button-div">
-      <button ><span>White Paper</span></button>
+        <button>
+          <span>White Paper</span>
+        </button>
       </div>
-      <div className="second-div">
-        <img className="secondImg" src={secondImg} alt = "Freedom Blog"/>
+      <div className="second-section">
+        <div className="second-div">
+          <img className="secondImg" src={secondImg} alt="Freedom Blog" />
+        </div>
+        <div className="first-main">
+          <div className="para1">
+            <h2>
+              ANIMAL WORLD is a dynamic tokenized, environmentally sustainable
+              PROJECT IN THE FORM OF futuristic MULTI-CHAIN farM BUILDING game.
+              It offers a riveting simulated gameplay iN which a player can
+              collect, own, trade and exchange farming assets and winnings. We
+              at ANIMAL WORLD are committed towards enabling and building a
+              community of ecologically aware individuals who will engage in a
+              self-sustainable and ecologically positive farming. The project
+              runs on a real-time basis with user-friendly accessibility and
+              consistent monitoring. kitchen_footer-desklarge.png
+            </h2>
+          </div>
+          <div className="third-div">
+            <img  src={thegame} alt="The-Game"/>
+          </div>
+        </div>
       </div>
     </div>
   );
